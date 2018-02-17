69dc52120d2eb33344bbb5eb4a3a327f86a9feb1
Devin Sova <neoskeeter7@gmail.com>
2018-02-17 15:25:11 -0500

    Changed GradientView to PastelView from Pod Pastel

--- MariGold/ViewController.swift
+++ MariGold/ViewController.swift
@@ -7,19 +7,23 @@
 //
 
 import UIKit
+import Pastel
 
 class ViewController: UIViewController {
-
+	@IBOutlet var pastelView: PastelView!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+		
+		//Setup PastelView
+		pastelView.setColors([UIColor(red: 240/255, green: 138/255, blue: 1/255, alpha: 1.0),
+							  UIColor(red: 249/255, green: 212/255, blue: 0/255, alpha: 1.0)])
+		pastelView.startAnimation()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
