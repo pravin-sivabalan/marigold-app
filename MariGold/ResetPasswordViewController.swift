88a7164b3ed58b24d20fb915f064bd2fdb52a431
Devin Sova <neoskeeter7@gmail.com>
2018-02-19 11:59:58 -0500

    Added Reset Password View

---
+++ MariGold/ResetPasswordViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResetPasswordViewController.swift
+//  MariGold
+//
+//  Created by Devin Sova on 2/19/18.
+//  Copyright © 2018 MariGold. All rights reserved.
+//
+
+import UIKit
+
+class ResetPasswordViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+}
