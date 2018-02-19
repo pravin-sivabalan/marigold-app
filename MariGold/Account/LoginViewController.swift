0d4f18c713e55dfa3b4954ed051a21926eefff6c
pravin-sivabalan <pravinsivabalan@gmail.com>
2018-02-18 20:44:27 -0500

    Add login view controller and add styling for fields

---
+++ MariGold/Account/LoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LoginViewController.swift
+//  
+//
+//  Created by Pravin Sivabalan on 2/17/18.
+//
+
+import UIKit
+import Pastel
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.view.backgroundColor = .clear
+
+        let pastelView = PastelView(frame: view.bounds)
+        pastelView.setColors([UIColor(red: 240/255, green: 138/255, blue: 1/255, alpha: 1.0),
+                              UIColor(red: 249/255, green: 212/255, blue: 0/255, alpha: 1.0)])
+        pastelView.startAnimation()
+        view.insertSubview(pastelView, at: 0)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
