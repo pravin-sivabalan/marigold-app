fc4c2cc4ba3d994901f1cbe40ffbff17c340767f
Devin Sova <neoskeeter7@gmail.com>
2018-03-04 22:00:49 -0500

    Created MedicationAdditionViewController and new Addition Screen in Storyboard

---
+++ MariGold/MedicationAdditionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MedicationAdditionViewController.swift
+//  MariGold
+//
+//  Created by Devin Sova on 3/4/18.
+//  Copyright © 2018 MariGold. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MedicationAdditionViewController: UIViewController {
+	
+}
