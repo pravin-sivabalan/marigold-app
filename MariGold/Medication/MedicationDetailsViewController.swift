54bc47f225979d8a6c2967f14676b9b157483f54
Devin Sova <neoskeeter7@gmail.com>
2018-03-25 10:53:47 -0400

    Made Edit Mode have editable fields

--- MariGold/Medication/MedicationDetailsViewController.swift
+++ MariGold/Medication/MedicationDetailsViewController.swift
@@ -9,11 +9,12 @@
 import UIKit
 
 class MedicationDetailsViewController: UITableViewController{
-	@IBOutlet var Name: UILabel!
-	@IBOutlet var Dosage: UILabel!
-	@IBOutlet var Quantity: UILabel!
-	@IBOutlet var RunOutDate: UILabel!
+	@IBOutlet var Name: UITextField!
+	@IBOutlet var Dosage: UITextField!
+	@IBOutlet var Quantity: UITextField!
+	@IBOutlet var RunOutDate: UITextField!
 	@IBOutlet var Temporary: UILabel!
+	@IBOutlet var TemporarySwitch: UISwitch!
 	
 	var medication: Medication!
 	var isEditingMedication = false
@@ -25,21 +26,24 @@ class MedicationDetailsViewController: UITableViewController{
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
+		populateFieldsWithOGMedication()
 		self.navigationItem.title = medication.name
-		Name.text = medication.name ?? "Undefined"
-		//Dosage.text = medication.
-		Quantity.text = String(medication.quantity)
-		RunOutDate.text = medication.run_out_date ?? "Undefined"
-		Temporary.text = String(medication.temporary)
 		
 		DoneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(setEditingMedication(sender:)))
 		CancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(setEditingMedication(sender:)))
 		EditButton = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(setEditingMedication(sender:)))
 		
 		self.navigationItem.setRightBarButton(EditButton, animated: false)
-		
 		self.tableView.reloadData()
+	}
+	
+	func populateFieldsWithOGMedication() {
+		Name.text = medication.name ?? "Undefined"
+		//Dosage.text = medication.
+		Quantity.text = String(medication.quantity)
+		RunOutDate.text = medication.run_out_date ?? "Undefined"
+		Temporary.text = String(medication.temporary)
+		TemporarySwitch.isOn = medication.temporary
 	}
 	
 	@objc func setEditingMedication(sender: UIBarButtonItem) {
@@ -49,15 +53,38 @@ class MedicationDetailsViewController: UITableViewController{
 			NSLog("Done Pressed!")
 		}
 		
+		if sender == CancelButton {
+			//Fix all fields
+			populateFieldsWithOGMedication()
+		}
+		
 		if(!isEditingMedication) {
 			self.navigationItem.setHidesBackButton(true, animated: true)
 			self.navigationItem.setLeftBarButton(CancelButton, animated: true)
 			self.navigationItem.setRightBarButton(DoneButton, animated: true)
+			
+			Name.isEnabled = true
+			Name.textAlignment = .left
+			Dosage.isEnabled = true
+			Dosage.textAlignment = .left
+			Quantity.isEnabled = true
+			Quantity.textAlignment = .left
+			TemporarySwitch.isHidden = false
+			Temporary.textAlignment = .left
 		}
 		else {
 			self.navigationItem.setRightBarButton(EditButton, animated: true)
 			self.navigationItem.setLeftBarButton(nil, animated: true)
 			self.navigationItem.setHidesBackButton(false, animated: true)
+			
+			Name.isEnabled = false
+			Name.textAlignment = .right
+			Dosage.isEnabled = false
+			Dosage.textAlignment = .right
+			Quantity.isEnabled = false
+			Quantity.textAlignment = .right
+			TemporarySwitch.isHidden = true
+			Temporary.textAlignment = .right
 		}
 		isEditingMedication = !isEditingMedication
 	}
