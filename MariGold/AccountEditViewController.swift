a5f03e21b5a5a44a6955bcb6306cf3b4cac2b01d
Devin Sova <neoskeeter7@gmail.com>
2018-03-26 10:47:18 -0400

    Created EditAccountView Files and Basic UI

---
+++ MariGold/AccountEditViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AccountEditViewController.swift
+//  MariGold
+//
+//  Created by Devin Sova on 3/26/18.
+//  Copyright © 2018 MariGold. All rights reserved.
+//
+
+import UIKit
+
+class AccountEditViewController: UITableViewController {
+	@IBOutlet var FirstName: UITextField!
+	@IBOutlet var LastName: UITextField!
+	@IBOutlet var Allergies: UITextField!
+	@IBOutlet var NFLSwitch: UISwitch!
+	@IBOutlet var NBASwitch: UISwitch!
+	@IBOutlet var NCAASwitch: UISwitch!
+}
