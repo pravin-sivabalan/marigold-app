51b4b0749a92dd91c3e4092138fa3556a7ce020c
pravin-sivabalan <pravinsivabalan@gmail.com>
2018-04-16 12:29:22 -0400

    Add refill notification

--- MariGold/MedicationAddition/AddNotificationTableViewController.swift
+++ MariGold/MedicationAddition/AddNotificationTableViewController.swift
@@ -45,25 +45,31 @@ class AddNotificationTableViewController: UITableViewController {
         }
         
         var body = med
-        let emailNotification = med["emailNotification"] as! Bool
-        let phoneNotification = med["emailNotification"] as! Bool
         
+        let emailNotification = med["emailNotification"] as! Bool
         if(emailNotification == true) {
             body["alert_user"] = "1"
         } else {
             body["alert_user"] = "0"
         }
         
+        let phoneNotification = med["phoneNotification"] as! Bool
         if(phoneNotification == true) {
             body["notifications"] = localNotifications
             Notify.createForMedication(medication: body)
         }
         
+        let refillNotification = med["refillNotification"] as! Bool
+        if(refillNotification == true) {
+            body["refill"] = 1
+        } else {
+            body["refill"] = 0
+        }
+        
         body["notifications"] = dataNotifications
         body.removeValue(forKey: "phoneNotification")
         body.removeValue(forKey: "emailNotification")
-        
-        print(body)
+        body.removeValue(forKey: "refillNotification")
         
         Alamofire.request(api.rootURL + "/meds/add", method: .post, parameters: body, encoding: JSONEncoding.default, headers: User.header).responseJSON { response in
             print(response)
